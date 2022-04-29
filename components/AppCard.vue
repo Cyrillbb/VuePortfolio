97c7a22c9a690009ebd513ac59d9e51a067da7cc
Baryshnikov.KS <Baryshnikov.KS@avgd.pro>
2022-04-29 23:33:58 +0400

    card styles

--- components/AppCard.vue
+++ components/AppCard.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="card">
-    <slot></slot>
+    <div class="card-header">
+      <slot name="card-header"></slot>
+    </div>
+    <div class="card-body">
+      <slot name="card-body"></slot>
+    </div>
   </div>
 </template>
 
@@ -15,13 +20,23 @@ export default {};
   margin: 25px auto;
   /* background: #24283b; */
   background: #1a1b26;
-  padding: 25px;
+  padding: 0 25px 25px;
   border-radius: 0.5em;
-  box-shadow: 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
-    0px 2px 1px -1px rgba(0, 0, 0, 0.12), 0px 1px 3px 0px rgba(0, 0, 0, 0.2);
+  box-shadow: 0px 1px 1px 0px rgb(0 0 0 / 14%),
+    0px 2px 1px -1px rgb(0 0 0 / 12%), 0px 1px 3px 0px rgb(0 0 0 / 20%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
 }
 
-.card h3 {
-  margin: 0;
+.card-header {
+  /* background: #414868; */
+  background: #5a4a78;
+  text-align: center;
+  width: 125%;
+  padding: 5px 0;
+  margin-bottom: 10px;
 }
 </style>
--- pages/experiences.vue
+++ pages/experiences.vue
@@ -1,48 +1,57 @@
 <template>
   <div class="experiences">
     <app-card>
-      I'm a Front-end developer with
-      experience in building web applications and websites. <br />
-      I specialize in JavaScript/TypeScript and Vue.js (2 and 3). Also have a
-      bit of experience in react.js, php and node.js.
-    </app-card>
-    <app-card>
-      <h3>Preferred tech-stack</h3>
-      <ul>
-        <li>Vue.js (vue 2 and vue 3), vuex, vue-router, vue-cli.</li>
-        <li>TypeScript</li>
-        <li>HTML, CSS, SCSS, Bootstrap, PrimeFaces</li>
-        <li>Docker</li>
-        <li>Php, node.js</li>
-      </ul>
+      <template #card-header> Hello header </template>
+      <template #card-body>
+        I'm a Front-end developer with experience in building web applications
+        and websites. <br />
+        I specialize in JavaScript/TypeScript and Vue.js (2 and 3). Also have a
+        bit of experience in react.js, php and node.js.
+      </template>
     </app-card>
 
     <app-card>
-      <h3>Experience</h3>
+      <template #card-header> Preferred tech-stack </template>
+      <template #card-body>
+        <ul>
+          <li>Vue.js (vue 2 and vue 3), vuex, vue-router, vue-cli.</li>
+          <li>TypeScript</li>
+          <li>HTML, CSS, SCSS, Bootstrap, PrimeFaces</li>
+          <li>Docker</li>
+          <li>Php, node.js</li>
+        </ul>
+      </template>
+    </app-card>
+
+    <app-card>
+      <template #card-header>Experience</template>
       <div>Front-end developer at chain pharmacy "Vita" (vitaexpress.ru)</div>
-      <ul>
-        <li>
-          Developed a pharmacy chain e-commerce customer-facing website as a
-          member of a 7-person team.
-        </li>
-        <li>
-          Created a library of ui-components (inputs, modals, form validation
-          logic etc.) as member of 2-person team.
-        </li>
-        <li>Created several promotion landing-pages.</li>
-        <li>
-          Implemented front-end logic along with 3rd-party software integrations
-          for a custmer-facing project.
-        </li>
-        <li>
-          Refactored several old modules with modern front-end technologies and
-          practices.
-        </li>
-        <li>
-          Created UI for several internal projects as a member of a 3-person
-          team.
-        </li>
-      </ul>
+
+      <template #card-body>
+        <ul>
+          <li>
+            Developed a pharmacy chain e-commerce customer-facing website as a
+            member of a 7-person team.
+          </li>
+          <li>
+            Created a library of ui-components (inputs, modals, form validation
+            logic etc.) as member of 2-person team.
+          </li>
+          <li>Created several promotion landing-pages.</li>
+          <li>
+            Implemented front-end logic along with 3rd-party software
+            integrations for a custmer-facing project.
+          </li>
+          <li>
+            Refactored several old modules with modern front-end technologies
+            and practices.
+          </li>
+          <li>
+            Created UI for several internal projects as a member of a 3-person
+            team.
+          </li>
+        </ul>
+      </template>
     </app-card>
   </div>
 </template>
